1ce3b9925b8edaa36b5c78f78a23c014a5b8e882
Eli Byrd <edbgkk@mst.edu>
2023-05-20 15:21:59 -0500

    Initialize CMake Project
    - Add .gitignore file
    - Add Logging System

---
+++ Source/Autonomy/Main/MRDTAutonomyGlobals.h
@@ -0,0 +1,27 @@
+/*
+   MRDTAutonomyGlobals.h
+   Copyright (c) 2023 Mars Rover Design Team. All rights reserved.
+
+   Date:             5/20/2023
+   Author:           Eli Byrd and Clayton Cowen
+   Description:      Defines global defines, variables, and functions for MRDT Software.
+*/
+
+#include <chrono>
+#include <ctime>
+#include <plog/Log.h>
+#include <plog/Initializers/RollingFileInitializer.h>
+#include <plog/Initializers/ConsoleInitializer.h>
+
+#ifndef MRDTAUTONOMYGLOBALS_H
+#define MRDTAUTONOMYGLOBALS_H
+
+// Logging:
+enum AutonomyLogger { AL_FileLogger, AL_ConsoleLogger };             // Enum to specify logging location
+
+extern plog::RollingFileAppender<plog::TxtFormatter> pFileAppender;  // Sends log message to file
+extern plog::ConsoleAppender<plog::TxtFormatter> pConsoleAppender;   // Sends log message to file and console
+
+void InitializeAutonomyLoggers();                                    // Method to set up the loggers
+
+#endif
