d8bd1e180c3be7e2750821caf558093eda627210
Eli Byrd <edbgkk@mst.edu>
2023-05-20 22:19:53 -0500

    Began Navigation Board Integration
    - Add Constructor
    - Add ProcessIMUData Function
    - Add ProcessGPSData Function
    - Add GetDData Function
    - Add GetSData Function
    - Add Notes for RoveComm Interface in the future

---
+++ Source/Autonomy/Communication/MRDT_NavigationBoard.h
@@ -0,0 +1,87 @@
+/*
+   MRDT_NavigationBoard.h
+   Copyright (c) 2023 Mars Rover Design Team. All rights reserved.
+
+   Date:             5/20/2023
+   Author:           Eli Byrd and Clayton Cowen
+   Description:      Interfaces with the Navigation Board over the RoveComm Protocol.
+*/
+
+#include <chrono>
+#include <ctime>
+
+#ifndef MRDT_NAVIGATIONBOARD_H
+#define MRDT_NAVIGATIONBOARD_H
+
+struct NavBoardPacket_IMU {
+    double      dPitch;
+    double      dRoll;
+    double      dHeading;
+
+    NavBoardPacket_IMU() {
+        dPitch    = 0.0;
+        dRoll     = 0.0;
+        dHeading  = 0.0;
+    }
+
+    NavBoardPacket_IMU(double dPitch, double dRoll, double dHeading) {
+        this->dPitch   = dPitch;
+        this->dRoll    = dRoll;
+        this->dHeading = dHeading;
+    }
+};
+
+struct NavBoardPacket_GPS {
+    double dLatitude;
+    double dLongitude;
+
+    NavBoardPacket_GPS() {
+        dLatitude  = 0.0;
+        dLongitude = 0.0;
+    }
+
+    NavBoardPacket_GPS(double dLatitude, double dLongitude) {
+        this->dLatitude  = dLatitude;
+        this->dLongitude = dLongitude;
+    }
+};
+
+enum NavigationBoardPacketDoubleComponents {
+    NBPC_PITCH,
+    NBPC_ROLL,
+    NBPC_HEADING
+};
+
+enum NavigationBoardPacketCoordinateComponents {
+    NBPCC_LOCATION
+};
+
+class MRDT_NavigationBoard {
+private:
+    int m_iDistanceToGround;
+    int m_iLidarQuality;
+
+    double m_dPitch;
+    double m_dRoll;
+    double m_dHeading;
+
+    time_t m_tLastTime;
+
+    NavBoardPacket_GPS m_sLocation;
+
+    // TODO: RoveComm Node
+
+    // TODO: RoveComm Callback -> IMU Data
+    // TODO: RoveComm Callback -> GPS Data
+
+public:
+    MRDT_NavigationBoard();
+
+    void ProcessIMUData(NavBoardPacket_IMU packet);
+    void ProcessGPSData(NavBoardPacket_GPS packet);
+
+    double GetDData(NavigationBoardPacketDoubleComponents eKey) const;
+    NavBoardPacket_GPS GetSData(NavigationBoardPacketCoordinateComponents eKey) const;
+};
+
+#endif // MRDT_NAVIGATIONBOARD_H
