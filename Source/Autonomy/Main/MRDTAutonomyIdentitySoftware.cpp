b9f69982763ac4e2856614020c395ecebd5bc420
Eli Byrd <edbgkk@mst.edu>
2023-05-20 19:35:14 -0500

    Add Software Versioning
    - Add Identity Software Class
    - Add Build Numbers Increment File for GitHub Action

---
+++ Source/Autonomy/Main/MRDTAutonomyIdentitySoftware.cpp
@@ -0,0 +1,54 @@
+/*
+   MRDTAutonomyIdentitySoftware.cpp
+   Copyright (c) 2023 Mars Rover Design Team. All rights reserved.
+
+   Date:             5/20/2023
+   Author:           Eli Byrd and Clayton Cowen
+   Description:      Defines global defines, variables, and functions for MRDT Software.
+*/
+
+#include "MRDTAutonomyIdentitySoftware.h"
+
+MRDTAutonomyIdentitySoftware::MRDTAutonomyIdentitySoftware() {
+
+    // Force extra characters to the version numbers for '0' padding
+    szMajorVersion = "000";
+    szMinorVersion = "000";
+    szPatchVersion = "000";
+    szBuildVersion = "000";
+
+    // Append the versions after the forced extra characters
+    szMajorVersion += std::to_string(MAJOR_VERSION);
+    szMinorVersion += std::to_string(MINOR_VERSION);
+    szPatchVersion += std::to_string(PATCH_VERSION);
+    szBuildVersion += std::to_string(BUILD_VERSION);
+
+    // Shorten the strings down to the appropriate lengths
+    if (szMajorVersion.length() > 2) {
+        szMajorVersion.erase(0, szMajorVersion.length() - 2);
+    }
+
+    if (szMinorVersion.length() > 2) {
+        szMinorVersion.erase(0, szMinorVersion.length() - 2);
+    }
+
+    if (szPatchVersion.length() > 2) {
+        szPatchVersion.erase(0, szPatchVersion.length() - 2);
+    }
+
+    if (szBuildVersion.length() > 3) {
+        szBuildVersion.erase(0, szBuildVersion.length() - 3);
+    }
+}
+
+std::string MRDTAutonomyIdentitySoftware::GetVersionNumber() {
+    return "v" + szMajorVersion + "." + szMinorVersion + "." + szPatchVersion;
+}
+
+std::string MRDTAutonomyIdentitySoftware::GetBuildNumber() {
+    return  "Build " + szBuildVersion;
+}
+
+std::string MRDTAutonomyIdentitySoftware::GetVersionBuildComboNumber() {
+    return  "v" + szMajorVersion + "." + szMinorVersion + "." + szPatchVersion + " Build " + szBuildVersion;
+}
