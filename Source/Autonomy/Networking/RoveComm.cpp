c5e48d492223f1efc61d990c25fcb73d1abb42ff
Eli Byrd <edbgkk@mst.edu>
2023-05-22 21:07:38 -0500

    RoveComm C++ Development - 5/22/2023 Save
    - Referencing RoveComm in Python Repository
    - Created RoveCommPacket Object
    - Ready for the development of RoveComm Class

---
+++ Source/Autonomy/Networking/RoveComm.cpp
@@ -0,0 +1,10 @@
+/*
+   RoveComm.cpp
+   Copyright (c) 2023 Mars Rover Design Team. All rights reserved.
+
+   Date:             5/21/2023
+   Author:           Eli Byrd and Clayton Cowen
+   Description:      
+*/
+
+#include "RoveComm.h"
