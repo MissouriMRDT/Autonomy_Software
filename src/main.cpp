0a9591f7677633a9d17208221f59ef92c0927b7c
Eli Byrd <edbgkk@mst.edu>
2023-07-24 01:23:44 -0500

    Update action and add temp memory leak

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,28 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string>
 
 #include "./AutonomyGlobals.h"
+
+char* create_banner()
+{
+    const char* user = getenv("USER");
+    size_t len       = 1 + 2 * 4 + strlen(user) + 1;
+    char* b          = (char*) malloc(len);
+    sprintf(b, "\t|** %s **|", user);
+    return b;
+}
+
+void output_report(int nr)
+{
+    char* banner = create_banner();
+    puts(banner);
+    printf("Number: %d\n", nr);
+    printf("\n");
+}
 
 /******************************************************************************
  * @brief Autonomy main function.
@@ -44,6 +63,10 @@ int main()
     // TODO: Initialize Threads
 
     // TODO: Initialize RoveComm
+
+    // Create Memory Leak
+    for (int i = 1; i <= 3; i++)
+        output_report(i);
 
     return 0;
 }
