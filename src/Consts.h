f58b2de905ab49894414cb60a8301a92b2b39b79
Eli Byrd <edbgkk@mst.edu>
2023-07-30 18:40:14 -0500

    Switch to constants header

---
+++ src/Consts.h
@@ -0,0 +1,18 @@
+/******************************************************************************
+ * @brief The constants header for Autonomy Software
+ *
+ * @file Consts.h
+ * @author Eli Byrd (edbgkk@mst.edu)
+ * @date 2023-07-30
+ *
+ * @copyright Copyright MRDT 2023 - All Rights Reserved
+ ******************************************************************************/
+
+#ifndef CONSTS_H
+#define CONSTS_H
+
+// Drive Constants
+#define MAX_DRIVE_POWER 250
+#define MIN_DRIVE_POWER 50
+
+#endif    // CONSTS_H
