634749244d0dd1fa118e922f519bc9ed5c3b9bab
Eli Byrd <edbgkk@mst.edu>
2023-08-14 21:51:15 -0500

    Switch to OS Version Numbered Packages

--- tools/docker/increment_jetpack.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# This script updates everywhere the 'autonomy-jetpack' image is used when a new version is available
-
-# Get Today's Date
-now="$(date +'%Y-%m-%d')"
-
-# Update devcontainer.json
-sed -i '' -E "s/autonomy-jetpack:.*\",/autonomy-jetpack:$now\",/" ../../.devcontainer/devcontainer.json
