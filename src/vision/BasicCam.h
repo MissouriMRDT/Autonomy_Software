b23ad712e569af0a438a762250c331fc21bbb8fa
clayjay3 <claytonraycowen@gmail.com>
2023-08-20 01:44:20 +0000

    Implemented basic camera.

--- src/vision/BasicCam.h
+++ src/vision/BasicCam.h
@@ -11,14 +11,42 @@
 #ifndef BASICCAM_H
 #define BASICCAM_H
 
+#include <opencv2/opencv.hpp>
+
 #include "../interfaces/Camera.hpp"
 
 class BasicCam : public Camera<cv::Mat>
 {
     private:
         // Declare private methods and functions variables.
+        bool m_bCameraIsConnectedOnVideoIndex;
+        int m_nCameraIndex;
+        std::string m_szCameraPath;
+        cv::VideoCapture m_cvCamera;
+        cv::Mat m_cvFrame;
 
     public:
         // Declare public methods and member variables.
+        BasicCam(const std::string szCameraPath,
+                 const int nPropResolutionX,
+                 const int nPropResolutionY,
+                 const int nPropFramesPerSecond,
+                 const PIXEL_FORMATS ePropPixelFormat,
+                 const double dPropHorizontalFOV,
+                 const double dPropVerticalFOV);
+        BasicCam(const int nCameraIndex,
+                 const int nPropResolutionX,
+                 const int nPropResolutionY,
+                 const int nPropFramesPerSecond,
+                 const PIXEL_FORMATS ePropPixelFormat,
+                 const double dPropHorizontalFOV,
+                 const double dPropVerticalFOV);
+        ~BasicCam();
+        cv::Mat GrabFrame() override;
+
+        // Getters.
+        template<typename T>
+        T GetCameraLocation();
+        bool GetCameraIsOpen() override;
 };
 #endif
