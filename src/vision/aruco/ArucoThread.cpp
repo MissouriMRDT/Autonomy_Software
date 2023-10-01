d85760c3dc76cfeb4c39a25f74633ccaa0536420
jspencerpittman <jspencerpittman@gmail.com>
2023-10-01 15:23:20 +0000

    Handled ArucoThread Fixme's

--- src/vision/aruco/ArucoThread.cpp
+++ src/vision/aruco/ArucoThread.cpp
@@ -1,16 +1,14 @@
 #include "ArucoThread.h"
 
-// FIXME: Remove CameraHandlerThread.
-ArucoThread::ArucoThread(CameraHandlerThread* cameraHandlerThread, const int nNumDetectedTagsRetrievalThreads) :
-    m_pCameraHandlerThread(cameraHandlerThread),
-    nNumDetectedTagsRetrievalThreads(nNumDetectedTagsRetrievalThreads)    // FIXME: Don't initialize class member variables like this. This method should only be used to
-                                                                          // initialize members of inherited classes. Move these to the constructor body.
+ArucoThread::ArucoThread(const int nNumDetectedTagsRetrievalThreads) :
 {
-    // initialize aruco detector
-    cv::aruco::Dictionary dictionary =
-        cv::aruco::getPredefinedDictionary(DEFAULT_DICTIONARY);    // Change this to use the new variable in the constants namespace. (constants::ARUCO_DICTIONARY)
+    m_nNumDetectedTagsRetrievalThreads = nNumDetectedTagsRetrievalThreads;
 
-    m_arucoDetector = ArucoDetector(dictionary);
+    // initialize aruco detector
+    cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(constants::ARUCO_DICTIONARY);
+    m_arucoDetector                  = ArucoDetector(dictionary);
+
+    m_pZedCam                        = g_pCameraHandler->GetZED(CameraHandlerThread::eHeadMainCam);
 }
 
 std::future<bool> ArucoThread::RequestDetectedArucoTags(std::vector<ArucoTag>& arucoTagVec)
@@ -19,26 +17,20 @@ std::future<bool> ArucoThread::RequestDetectedArucoTags(std::vector<ArucoTag>& a
     containers::DataFetchContainer<std::vector<ArucoTag>> stContainer(arucoTagVec);
 
     // Acquire lock on detected tags copy queue.
-    std::unique_lock<std::shared_mutex> lkScheduler(m_muPoolScheduleMutex);    // FIXME: Spelled wrong in header, make sure you are using devcontainer.
+    std::unique_lock<std::mutex> lkScheduler(m_muPoolScheduleMatrix);
     // Append detected tag fetch container to the schedule queue.
     m_qDetectedTagCopySchedule.push(stContainer);
     // Release lock on the frame schedule queue.
     lkScheduler.unlock();
 
     // Return the future from the promise stored in the container.
-    return stContainer.pCopiedFrameStatus->get_future();    // FIXME: Make sure you are developing within the container.
+    return stContainer.pCopiedDataStatus->get_future();
 }
 
-void ArucoThread::ThreadContinousCode()
+void ArucoThread::ThreadedContinuousCode()
 {
-    // TODO: better handling of zedCamera pointer
     // TODO: support pose estimation-this would include depth maps
     // TODO: pass reference to vector to ArucoDetector.Detect instead
-
-    // Access zed camera
-    // FIXME: Change m_pCameraHandlerThread to g_pCameraHandler.
-    ZEDCam* zedCamera = m_pCameraHandlerThread->GetZED(CameraHandlerThread::eHeadMainCam);    // Add ZEDCam* m_pMainCam to header file and replace zedCam with m_pZedCam.
-    // FIXME: No need to get a pointer ever iteration. Just get the pointer once in the constructor and store it in a member variable.
 
     // hold camera frames
     cv::Mat cvNormalFrame;
@@ -48,15 +40,15 @@ void ArucoThread::ThreadContinousCode()
     std::future<bool> fuFrameCopyStatus;
 
     // Check if the camera is setup to use CPU or GPU mats.
-    if (constants::ZED_MAINCAM_USE_GPU_MAT)    // FIXME: You need to #include "AutonomyConstants.h" to access the constants namespace.
+    if (constants::ZED_MAINCAM_USE_GPU_MAT)
     {
         // Grab frames from camera.
-        fuFrameCopyStatus = zedCamera->RequestFrameCopy(cvGPUNormalFrame);
+        fuFrameCopyStatus = m_pZedCam->RequestFrameCopy(cvGPUNormalFrame);
     }
     else
     {
         // Grab frames from camera.
-        fuFrameCopyStatus = zedCamera->RequestFrameCopy(cvNormalFrame);
+        fuFrameCopyStatus = m_pZedCam->RequestFrameCopy(cvNormalFrame);
     }
 
     // Wait for the frames to be copied.
@@ -72,20 +64,20 @@ void ArucoThread::ThreadContinousCode()
         // Acquire lock on detected tags
         std::unique_lock<std::mutex> lkDetectedTags(m_muDetectedTagsMutex);
         // Update detected tags
-        m_detectedTags = m_arucoDetector.Detect(cvNormalFrame);    // FIXME: No need to aquire a lock since the detect function call is not running in a seperate thread.
+        m_vDetectedTags = m_arucoDetector.Detect(cvNormalFrame);    // FIXME: No need to aquire a lock since the detect function call is not running in a seperate thread.
         // Release lock on detected tags
         lkDetectedTags.unlock();
     }
 
     // Acquire a shared_lock on the detected tags copy queue.
-    std::shared_lock<std::shared_mutex> lkSchedulers(m_muPoolScheduleMutex);
+    std::shared_lock<std::mutex> lkSchedulers(m_muPoolScheduleMatrix);
     // Check if the detected tag copy queue is empty.
     if (!m_qDetectedTagCopySchedule.empty())
     {
         // FIXME: Won't compile.
-        size_t = siQueueLength = m_qDetectedTagCopySchedule.size();
+        size_t siQueueLength = m_qDetectedTagCopySchedule.size();
         // Start the thread pool to store multiple copies of the detected tags to the requesting threads
-        this->RunDetachedPool(siQueueLength, m_nNumFrameRetrievalThreads);
+        this->RunDetachedPool(siQueueLength, m_nNumDetectedTagsRetrievalThreads);
         // Wait for thread pool to finish.
         this->JoinPool();
         // Release lock on frame copy queue.
@@ -93,10 +85,10 @@ void ArucoThread::ThreadContinousCode()
     }
 }
 
-ArucoThread::PooledLinearCode()
+void ArucoThread::PooledLinearCode()
 {
     // Acquire sole writing access to the detectedTagCopySchedule
-    std::unique_lock<std::shared_mutex> lkTagQueue(m_muPoolScheduleMatrix);
+    std::unique_lock<std::mutex> lkTagQueue(m_muPoolScheduleMatrix);
 
     // If there are unfulfilled requests
     if (!m_qDetectedTagCopySchedule.empty())
@@ -108,23 +100,13 @@ ArucoThread::PooledLinearCode()
         // Release lock.
         lkTagQueue.unlock();
 
-        // Acquire sole writing access to the container
-        std::unique_lock<std::mutex> lkConditionLock(
-            stContainer
-                .muConditionMutex);    // FIXME: Once the container is copied out of the queue, you now have sole ownership of the resource. No need to aquire a lock.
-
         // Acquire reading access to the detected tags
         std::shared_lock<std::mutex> lkDetectedTags(m_muDetectedTagsMutex);
 
         // Copy the detected tags to the target location
-        stContainer.tData = m_vDetectedTags;    // FIXME: tData shoudl be pData and is a pointer, dereference it. *(stContainer.pData) = m_vDetectedTags;
+        *(stContainer.pData) = m_vDetectedTags;
 
         // Release locks
-        lkConditionLock.unlock();
         lkDetectedTags.unlock();
-
-        // Use the condition variable to notify other waiting threads that this thread is finished.
-        stContainer.cdMatWriteSuccess
-            .notify_all();    // FIXME: We don't use condition variables anymore, this is handled by promises and future. See BasicCam again for a good example.
     }
 }
