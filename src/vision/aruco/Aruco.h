ff34a8cd7b8db57ef685c0cdd7a7d2258d375a00
jspencerpittman <jspencerpittman@gmail.com>
2023-10-01 17:46:57 +0000

    Fixed general errors and addressed locking issues #67

--- src/vision/aruco/Aruco.h
+++ src/vision/aruco/Aruco.h
@@ -2,21 +2,17 @@
 #define ARUCO_DETECTOR_H_
 
 #include <algorithm>
-#include <future>          // FIXME: Remove this
-#include <shared_mutex>    // FIXME: Remove this
 #include <string>
 #include <vector>
 
 #include <opencv2/objdetect/aruco_detector.hpp>
 #include <opencv2/opencv.hpp>
 
+#include "./../../AutonomyConstants.h"
 #include "./../../interfaces/AutonomyThread.hpp"
 #include "./../../interfaces/Camera.hpp"
 #include "./../../util/vision/ArucoSamplesUtility.hpp"
 #include "./../../util/vision/FetchContainers.hpp"
-
-// FIXME: Never use #defines to define a literal. Always put any variables of a global scope in the AutonomyConstants file within the constants namespace.
-#define TAG_SIDE_LENGTH 1    // meters
 
 /******************************************************************************
  * @brief Represents a single ArUco tag.
@@ -27,13 +23,10 @@
  ******************************************************************************/
 struct ArucoTag
 {
-        // FIXME: typedefs are generally discouraged as it makes code harders to understand. Instead use a struct within a struct. Similar to ZedObjectData struct.
-        typedef std::vector<cv::Point2f> Corners;
-
-        int id;                   // ID of the tag
-        Corners corners;          // Corners of the tag in the image
-        double distance;          // Distance between the tag and the camera
-        cv::Vec3d eulerAngles;    // Euler angle rotations of the tag with respect to the camera
+        int id;                              // ID of the tag
+        std::vector<cv::Point2f> corners;    // Corners of the tag in the image
+        double distance;                     // Distance between the tag and the camera
+        cv::Vec3d eulerAngles;               // Euler angle rotations of the tag with respect to the camera
 };
 
 /******************************************************************************
@@ -50,14 +43,21 @@ class ArucoDetector
         /******************************************************************************
          * @brief Construct a new Aruco Detector object.
          *
+         * uses default 4x4x50 ArUco dictionary
+         *
+         * @author jspencerpittman (jspencerpittman@gmail.com)
+         * @date 2023-10-01
+         ******************************************************************************/
+        ArucoDetector();
+        /******************************************************************************
+         * @brief Construct a new Aruco Detector object.
+         *
          * @param dictionary - Which dictionary are the ArUco tags in (DICT_4x4_50, DICT_6x6_250, ...)
          *
          * @author jspencerpittman (jspencerpittman@gmail.com)
          * @date 2023-09-28
          ******************************************************************************/
         ArucoDetector(const cv::aruco::Dictionary& dictionary);
-
-        // FIXME: This can be put within a private method inside of the ArucoVision class.
         /******************************************************************************
          * @brief detect ArUco tags in the provided image
          *
@@ -68,8 +68,6 @@ class ArucoDetector
          * @date 2023-09-28
          ******************************************************************************/
         std::vector<ArucoTag> Detect(const cv::Mat& image);
-
-        // FIXME: This can be put within a private method inside of the ArucoVision class.
         /******************************************************************************
          * @brief estimate the tag's position and euler angle rotation with respect to camera
          *
@@ -93,7 +91,7 @@ class ArucoDetector
          * @author jspencerpittman (jspencerpittman@gmail.com)
          * @date 2023-09-28
          ******************************************************************************/
-        static cv::Vec3d AxisAngleRotation2EulerAngles(cv::Vec3d rotVec) const;
+        static cv::Vec3d AxisAngleRotation2EulerAngles(cv::Vec3d rotVec);
 
         cv::aruco::Dictionary m_cvDictionary;
         std::string m_szCameraParamsFilename;
