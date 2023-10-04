be50710f25f7006f56ba46229a1b448ba5a254d2
jspencerpittman <jspencerpittman@gmail.com>
2023-10-04 21:30:39 +0000

    Converted ArucoDetector to a namespace 'aruco' #67

--- src/vision/aruco/Aruco.h
+++ src/vision/aruco/Aruco.h
@@ -14,87 +14,33 @@
 #include "./../../util/vision/ArucoSamplesUtility.hpp"
 #include "./../../util/vision/FetchContainers.hpp"
 
-/******************************************************************************
- * @brief Represents a single ArUco tag.
- *
- *
- * @author jspencerpittman (jspencerpittman@gmail.com)
- * @date 2023-09-28
- ******************************************************************************/
-struct ArucoTag
+namespace aruco
 {
-        int id;                              // ID of the tag
-        std::vector<cv::Point2f> corners;    // Corners of the tag in the image
-        double distance;                     // Distance between the tag and the camera
-        cv::Vec3d eulerAngles;               // Euler angle rotations of the tag with respect to the camera
-};
+    /******************************************************************************
+     * @brief Represents a single ArUco tag.
+     *
+     *
+     * @author jspencerpittman (jspencerpittman@gmail.com)
+     * @date 2023-09-28
+     ******************************************************************************/
+    struct ArucoTag
+    {
+            int id;                              // ID of the tag
+            std::vector<cv::Point2f> corners;    // Corners of the tag in the image
+            double distance;                     // Distance between the tag and the camera
+            cv::Vec3d eulerAngles;               // Euler angle rotations of the tag with respect to the camera
+    };
 
-/******************************************************************************
- * @brief Provides the ability to detect tags in an image as well as estimating
- *          the detected tag's position
- *
- *
- * @author jspencerpittman (jspencerpittman@gmail.com)
- * @date 2023-09-28
- ******************************************************************************/
-class ArucoDetector
-{
-    public:
-        /******************************************************************************
-         * @brief Construct a new Aruco Detector object.
-         *
-         * uses default 4x4x50 ArUco dictionary
-         *
-         * @author jspencerpittman (jspencerpittman@gmail.com)
-         * @date 2023-10-01
-         ******************************************************************************/
-        ArucoDetector();
-        /******************************************************************************
-         * @brief Construct a new Aruco Detector object.
-         *
-         * @param dictionary - Which dictionary are the ArUco tags in (DICT_4x4_50, DICT_6x6_250, ...)
-         *
-         * @author jspencerpittman (jspencerpittman@gmail.com)
-         * @date 2023-09-28
-         ******************************************************************************/
-        ArucoDetector(const cv::aruco::Dictionary& dictionary);
-        /******************************************************************************
-         * @brief detect ArUco tags in the provided image
-         *
-         * @param image - image containing the ArUco tags
-         * @return std::vector<ArucoTag> -
-         *
-         * @author jspencerpittman (jspencerpittman@gmail.com)
-         * @date 2023-09-28
-         ******************************************************************************/
-        std::vector<ArucoTag> Detect(const cv::Mat& image);
-        /******************************************************************************
-         * @brief estimate the tag's position and euler angle rotation with respect to camera
-         *
-         * @param tag - tag were estimating the pose of
-         *
-         * @author jspencerpittman (jspencerpittman@gmail.com)
-         * @date 2023-09-28
-         ******************************************************************************/
-        void EstimatePose(ArucoTag& tag);
-
-    private:
-        // FIXME: This could probably be put within the number operations namespace and generalized to accept and return something other than cv::Vec3d. Just an idea.
-        /******************************************************************************
-         * @brief convert axis angle rotations to euler angles
-         *
-         * @param rotVec - A 3-dimensionsal vector of axis-angle-rotations where
-         *                  the L2 norm of the vector is the angle rotated about the axis
-         *                  in the direction of vector's unit vector.
-         * @return cv::Vec3d - euler angles in the order of pitch, yaw, roll
-         *
-         * @author jspencerpittman (jspencerpittman@gmail.com)
-         * @date 2023-09-28
-         ******************************************************************************/
-        static cv::Vec3d AxisAngleRotation2EulerAngles(cv::Vec3d rotVec);
-
-        cv::aruco::Dictionary m_cvDictionary;
-        std::string m_szCameraParamsFilename;
-};
+    /******************************************************************************
+     * @brief detect ArUco tags in the provided image
+     *
+     * @param image - camera frame
+     * @return std::vector<ArucoTag> - vector of the detected tags in the frame
+     *
+     * @author jspencerpittman (jspencerpittman@gmail.com)
+     * @date 2023-09-28
+     ******************************************************************************/
+    std::vector<ArucoTag> Detect(const cv::Mat& image);
+}    // namespace aruco
 
 #endif
