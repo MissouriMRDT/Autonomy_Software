d1d53669218f9b39e37f5141d652a55b15a8af02
clayjay3 <claytonraycowen@gmail.com>
2023-10-08 02:47:31 +0000

    Wrote TagDetectionHandler.

---
+++ src/handlers/TagDetectionHandler.cpp
@@ -0,0 +1,93 @@
+/******************************************************************************
+ * @brief Implements the TagDetectionHandler class.
+ *
+ * @file TagDetectionHandler.cpp
+ * @author ClayJay3 (claytonraycowen@gmail.com)
+ * @date 2023-10-07
+ *
+ * @copyright Copyright MRDT 2023 - All Rights Reserved
+ ******************************************************************************/
+
+#include "TagDetectionHandler.h"
+#include "../AutonomyConstants.h"
+#include "../AutonomyGlobals.h"
+
+/******************************************************************************
+ * @brief Construct a new TagDetectionHandler::TagDetectionHandler object.
+ *
+ *
+ * @author ClayJay3 (claytonraycowen@gmail.com)
+ * @date 2023-10-07
+ ******************************************************************************/
+TagDetectionHandler::TagDetectionHandler()
+{
+    // Initialize detector for main ZEDcam.
+    m_pTagDetectorMainCam = new TagDetector(globals::g_pCameraHandler->GetZED(CameraHandler::eHeadMainCam),
+                                            constants::TAGDETECT_MAINCAM_DATA_RETRIEVAL_THREADS,
+                                            constants::ZED_MAINCAM_USE_GPU_MAT);
+}
+
+/******************************************************************************
+ * @brief Destroy the TagDetectionHandler::TagDetectionHandler object.
+ *
+ *
+ * @author ClayJay3 (claytonraycowen@gmail.com)
+ * @date 2023-10-07
+ ******************************************************************************/
+TagDetectionHandler::~TagDetectionHandler()
+{
+    // Signal and wait for cameras to stop.
+    this->StopAllDetectors();
+
+    // Delete dynamic memory.
+    delete m_pTagDetectorMainCam;
+
+    // Set dangling pointers to nullptr.
+    m_pTagDetectorMainCam = nullptr;
+}
+
+/******************************************************************************
+ * @brief Signals all detectors to start their threads.
+ *
+ *
+ * @author ClayJay3 (claytonraycowen@gmail.com)
+ * @date 2023-10-07
+ ******************************************************************************/
+void TagDetectionHandler::StartAllDetectors()
+{
+    // Start ZED maincam detector.
+    m_pTagDetectorMainCam->Start();
+}
+
+/******************************************************************************
+ * @brief Signals all detectors to stop their threads.
+ *
+ *
+ * @author ClayJay3 (claytonraycowen@gmail.com)
+ * @date 2023-10-07
+ ******************************************************************************/
+void TagDetectionHandler::StopAllDetectors()
+{
+    // Stop ZED maincam detector.
+    m_pTagDetectorMainCam->RequestStop();
+    m_pTagDetectorMainCam->Join();
+}
+
+/******************************************************************************
+ * @brief Accessor for TagDetector detectors.
+ *
+ * @param eDetectorName - The name of the detector to retrieve. An enum defined in and specific to this class.
+ * @return TagDetector* - A pointer to the detector pertaining to the given name.
+ *
+ * @author clayjay3 (claytonraycowen@gmail.com)
+ * @date 2023-10-07
+ ******************************************************************************/
+TagDetector* TagDetectionHandler::GetTagDetector(TagDetectors eDetectorName)
+{
+    // Determine which tag detector should be returned.
+    switch (eDetectorName)
+    {
+        case eHeadMainCam: return m_pTagDetectorMainCam;
+        default: return m_pTagDetectorMainCam;
+    }
+}
