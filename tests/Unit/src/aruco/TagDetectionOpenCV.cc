25f7cec7ce707328a81215a6a3a0046041073228
JSpencerPittman <jspencerpittman@gmail.com>
2023-10-14 04:09:34 +0000

    Created multiple aruco tag unit tests #67

--- tests/Unit/src/aruco/TagDetectionOpenCV.cc
+++ tests/Unit/src/aruco/TagDetectionOpenCV.cc
@@ -39,25 +39,32 @@ bool PointsAreEqual(const cv::Point_<T>& p1, const cv::Point_<T>& p2)
     return p1.x == p2.x && p1.y == p2.y;
 }
 
+cv::Mat LoadImageFromRelativePath(const std::string& relativePath)
+{
+    std::filesystem::path pathParentDir = std::filesystem::path(__FILE__).parent_path();
+    std::filesystem::path pathImage     = pathParentDir / relativePath;
+    return cv::imread(pathImage, cv::IMREAD_COLOR);
+}
+
 /******************************************************************************
- * @brief Test the functionality of the FindTagCenter method
+ * @brief Test the functionality of the FindTagcvCenterPoint method
  *
  *
  * @author JSpencerPittman (jspencerpittman@gmail.com)
  * @date 2023-10-11
  ******************************************************************************/
-TEST(TagDetectOpenCVTest, FindCenter)
+TEST(TagDetectOpenCVTest, FindTagCenter)
 {
     arucotag::ArucoTag tag;
-    tag.CornerTL                = cv::Point2f{7.0, 5.0};
-    tag.CornerTR                = cv::Point2f{10.0, 8.0};
-    tag.CornerBL                = cv::Point2f{3.0, 1.0};
-    tag.CornerBR                = cv::Point2f{7.0, 0.0};
+    tag.CornerTL                       = cv::Point2f{7.0, 5.0};
+    tag.CornerTR                       = cv::Point2f{10.0, 8.0};
+    tag.CornerBL                       = cv::Point2f{3.0, 1.0};
+    tag.CornerBR                       = cv::Point2f{7.0, 0.0};
 
-    cv::Point2f predictedCenter = FindTagCenter(tag);
+    cv::Point2f cvPredictedCenterPoint = FindTagCenter(tag);
 
-    cv::Point2f expectedCenter{6.75, 3.5};
-    EXPECT_PRED2(PointsAreEqual<float>, predictedCenter, expectedCenter);
+    cv::Point2f cvExpectedCenterPoint{6.75, 3.5};
+    EXPECT_PRED2(PointsAreEqual<float>, cvPredictedCenterPoint, cvExpectedCenterPoint);
 }
 
 /******************************************************************************
@@ -70,26 +77,25 @@ TEST(TagDetectOpenCVTest, FindCenter)
 TEST(TagDetectOpenCVTest, SingleCleanTagDetect)
 {
     // initialize aruco detector
-    cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(constants::ARUCO_DICTIONARY);
-    cv::aruco::ArucoDetector detector(dictionary);
+    cv::aruco::Dictionary cvDictionary = cv::aruco::getPredefinedDictionary(constants::ARUCO_DICTIONARY);
+    cv::aruco::ArucoDetector detector(cvDictionary);
 
-    std::filesystem::path pathParentDir = std::filesystem::path(__FILE__).parent_path();
-    std::filesystem::path pathImage     = pathParentDir / "rsc/cleanArucoMarker0.png";
-    cv::Mat testImage                   = cv::imread(pathImage, cv::IMREAD_COLOR);
+    // Load the image containing the sample ArUco tag
+    cv::Mat cvTestImageMat = LoadImageFromRelativePath("rsc/cleanArucoMarker0.png");
 
-    std::vector<arucotag::ArucoTag> detectedTags;
-    detectedTags = arucotag::Detect(testImage, detector);
+    // Detect tags in the image
+    std::vector<arucotag::ArucoTag> vecDetectedTags;
+    vecDetectedTags = arucotag::Detect(cvTestImageMat, detector);
 
     // Should have only detected one tag
-    ASSERT_GT(detectedTags.size(), 0);
-    EXPECT_EQ(detectedTags.size(), 1);
+    ASSERT_EQ(vecDetectedTags.size(), 1);
 
-    arucotag::ArucoTag detTag = detectedTags[0];
+    arucotag::ArucoTag detTag = vecDetectedTags[0];
 
     // Verify ID is 0
     EXPECT_EQ(detTag.nID, 0);
 
-    // Verify corners
+    // Does the detected tag's corners match with the real tags
     cv::Point2f expectedCornerTL{220, 220};
     cv::Point2f expectedCornerTR{419, 220};
     cv::Point2f expectedCornerBL{220, 419};
@@ -99,4 +105,101 @@ TEST(TagDetectOpenCVTest, SingleCleanTagDetect)
     EXPECT_PRED2(PointsAreEqual<float>, expectedCornerTR, detTag.CornerTR);
     EXPECT_PRED2(PointsAreEqual<float>, expectedCornerBL, detTag.CornerBL);
     EXPECT_PRED2(PointsAreEqual<float>, expectedCornerBR, detTag.CornerBR);
+}
+
+/******************************************************************************
+ * @brief Tests if the Detect method can find multiple tags and properly classify their vecIDS
+ *
+ *
+ * @author JSpencerPittman (jspencerpittman@gmail.com)
+ * @date 2023-10-11
+ ******************************************************************************/
+TEST(TagDetectOpenCVTest, MultiCleanTagDetect)
+{
+    // Number of tags for this test case
+    const unsigned int unNumTags = 3;
+
+    // initialize aruco detector
+    cv::aruco::Dictionary cvDictionary = cv::aruco::getPredefinedDictionary(constants::ARUCO_DICTIONARY);
+    cv::aruco::ArucoDetector detector(cvDictionary);
+
+    // Load the image containing the sample ArUco tags
+    cv::Mat cvTestImageMat = LoadImageFromRelativePath("rsc/cleanArucoMarkersMultiple.png");
+
+    // Detect tags in the image
+    std::vector<arucotag::ArucoTag> vecDetectedTags;
+    vecDetectedTags = arucotag::Detect(cvTestImageMat, detector);
+
+    // Should have three detected tags
+    ASSERT_EQ(vecDetectedTags.size(), unNumTags);
+
+    // Actual values for the tags in the image
+    std::vector<int> vecIDS{2, 3, 4};
+    std::vector<cv::Point2f> vecCenters{{100, 100}, {320, 480}, {500, 200}};
+    std::vector<int> vecSizes{100, 150, 200};
+
+    unsigned int unCorrectlyIdentifiedTags   = 0;
+    unsigned int unIncorrectlyIdentifiedTags = 0;
+
+    // For each detected tag check its one of the real tags
+    for (auto& detTag : vecDetectedTags)
+    {
+        // Which real tag does it match
+        int nTagIdx = -1;
+        for (int i = 0; i < (int) vecIDS.size(); ++i)
+        {
+            if (vecIDS[i] == detTag.nID)
+            {
+                // Found the corresponding real tag
+                nTagIdx = i;
+                break;
+            }
+        }
+        if (nTagIdx == -1)
+        {
+            // If no matching ID was found this was a falsely detected tag
+            ++unIncorrectlyIdentifiedTags;
+            continue;
+        }
+
+        cv::Point2f cvCenterPoint = vecCenters[nTagIdx];
+        unsigned int unApothem    = vecSizes[nTagIdx] / 2;    // Half the length of a square's side
+
+        // Calculate the expected corners
+        cv::Point2f expectedCornerTL{cvCenterPoint.x - unApothem, cvCenterPoint.y - unApothem};
+        cv::Point2f expectedCornerTR{cvCenterPoint.x + unApothem - 1, cvCenterPoint.y - unApothem};
+        cv::Point2f expectedCornerBL{cvCenterPoint.x - unApothem, cvCenterPoint.y + unApothem - 1};
+        cv::Point2f expectedCornerBR{cvCenterPoint.x + unApothem - 1, cvCenterPoint.y + unApothem - 1};
+
+        // Do the corners between the expected and detected tags match?
+        bool bTLMatch, bTRMatch, bBLMatch, bBRMatch;
+        bTLMatch = PointsAreEqual<float>(expectedCornerTL, detTag.CornerTL);
+        bTRMatch = PointsAreEqual<float>(expectedCornerTR, detTag.CornerTR);
+        bBLMatch = PointsAreEqual<float>(expectedCornerBL, detTag.CornerBL);
+        bBRMatch = PointsAreEqual<float>(expectedCornerBR, detTag.CornerBR);
+
+        EXPECT_TRUE(bTLMatch);
+        EXPECT_TRUE(bTRMatch);
+        EXPECT_TRUE(bBLMatch);
+        EXPECT_TRUE(bBRMatch);
+
+        if (bTLMatch && bTRMatch && bBLMatch && bBRMatch)
+        {
+            // Since this tag has been found remove it from the vector of real tags remaining
+            ++unCorrectlyIdentifiedTags;
+            vecIDS.erase(vecIDS.begin() + nTagIdx);
+            vecCenters.erase(vecCenters.begin() + nTagIdx);
+            vecSizes.erase(vecSizes.begin() + nTagIdx);
+        }
+        else
+        {
+            // Since the corner's don't match this was a falsely detected tag
+            ++unIncorrectlyIdentifiedTags;
+        }
+    }
+
+    // Were all of the tags identified?
+    ASSERT_EQ(unCorrectlyIdentifiedTags, unNumTags);
+    // Were there no false detections?
+    ASSERT_EQ(unIncorrectlyIdentifiedTags, 0);
 }
--- tests/Unit/src/aruco/rsc/Images.md
+++ tests/Unit/src/aruco/rsc/Images.md
@@ -1,4 +1,5 @@
 # Aruco Unit Test Images
+On the CENTER it is (x,y) not (r,c)
 
 **cleanArucoMarker0.png**
 Image Size: 640x640 pixels
@@ -7,4 +8,14 @@ Tag Count: 1
 Tags:
 ID: [0]
 CENTER: [(320,320)]
+SIZE: [200]
+
+
+**cleanArucoMarkerMultiple.png**
+Image Size: 640x640 pixels
+Tag Count: 3
+
+Tags:
+ID: [2, 3, 4]
+CENTER: [(100,100), (320, 480), (500,200)]
 SIZE: [200]
