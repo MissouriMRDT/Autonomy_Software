0f310cfd4b32f0b884f2a6c983896875f130115c
clayjay3 <claytonraycowen@gmail.com>
2023-11-13 03:02:59 +0000

    Change all tag detectors to use Aruco3.

--- src/AutonomyConstants.h
+++ src/AutonomyConstants.h
@@ -151,25 +151,25 @@ namespace constants
     const int TAGDETECT_MAINCAM_DATA_RETRIEVAL_THREADS  = 5;     // The number of threads allocated to the threadpool for performing data copies to other threads.
     const int TAGDETECT_MAINCAM_CORNER_REFINE_MAX_ITER  = 30;    // The maximum number of iterations to run corner refinement on the image.
     const int TAGDETECT_MAINCAM_CORNER_REFINE_METHOD    = cv::aruco::CORNER_REFINE_NONE;    // Algorithm used to refine tag corner pixels.
-    const bool TAGDETECT_MAINCAM_DETECT_INVERTED_MARKER = true;                             // Whether or not to detector upside-down tags.
-    const int TAGDETECT_MAINCAM_MARKER_BORDER_BITS      = 1;                                // This number of bits on the border. A bit is one unit square of the tag.
-    const bool TAGDETECT_MAINCAM_USE_ARUCO3_DETECTION   = false;                            // Whether or not to use the newer and faster Aruco detection strategy.
+    const bool TAGDETECT_MAINCAM_DETECT_INVERTED_MARKER = true;                               // Whether or not to detector upside-down tags.
+    const int TAGDETECT_MAINCAM_MARKER_BORDER_BITS      = 1;                                  // This number of bits on the border. A bit is one unit square of the tag.
+    const bool TAGDETECT_MAINCAM_USE_ARUCO3_DETECTION   = true;                               // Whether or not to use the newer and faster Aruco detection strategy.
 
     // Left Side Cam.
     const int TAGDETECT_LEFTCAM_DATA_RETRIEVAL_THREADS  = 5;     // The number of threads allocated to the threadpool for performing data copies to other threads.
     const int TAGDETECT_LEFTCAM_CORNER_REFINE_MAX_ITER  = 30;    // The maximum number of iterations to run corner refinement on the image.
-    const int TAGDETECT_LEFTCAM_CORNER_REFINE_METHOD    = cv::aruco::CORNER_REFINE_NONE;    // Algorithm used to refine tag corner pixels.
-    const bool TAGDETECT_LEFTCAM_DETECT_INVERTED_MARKER = true;                             // Whether or not to detector upside-down tags.
-    const int TAGDETECT_LEFTCAM_MARKER_BORDER_BITS      = 1;                                // This number of bits on the border. A bit is one unit square of the tag.
-    const bool TAGDETECT_LEFTCAM_USE_ARUCO3_DETECTION   = true;                             // Whether or not to use the newer and faster Aruco detection strategy.
+    const int TAGDETECT_LEFTCAM_CORNER_REFINE_METHOD    = cv::aruco::CORNER_REFINE_NONE;       // Algorithm used to refine tag corner pixels.
+    const bool TAGDETECT_LEFTCAM_DETECT_INVERTED_MARKER = true;                                // Whether or not to detector upside-down tags.
+    const int TAGDETECT_LEFTCAM_MARKER_BORDER_BITS      = 1;                                   // This number of bits on the border. A bit is one unit square of the tag.
+    const bool TAGDETECT_LEFTCAM_USE_ARUCO3_DETECTION   = true;                                // Whether or not to use the newer and faster Aruco detection strategy.
 
     // Right Side Cam.
     const int TAGDETECT_RIGHTCAM_DATA_RETRIEVAL_THREADS  = 5;     // The number of threads allocated to the threadpool for performing data copies to other threads.
     const int TAGDETECT_RIGHTCAM_CORNER_REFINE_MAX_ITER  = 30;    // The maximum number of iterations to run corner refinement on the image.
-    const int TAGDETECT_RIGHTCAM_CORNER_REFINE_METHOD    = cv::aruco::CORNER_REFINE_NONE;    // Algorithm used to refine tag corner pixels.
-    const bool TAGDETECT_RIGHTCAM_DETECT_INVERTED_MARKER = true;                             // Whether or not to detector upside-down tags.
-    const int TAGDETECT_RIGHTCAM_MARKER_BORDER_BITS      = 1;                                // This number of bits on the border. A bit is one unit square of the tag.
-    const bool TAGDETECT_RIGHTCAM_USE_ARUCO3_DETECTION   = false;                            // Whether or not to use the newer and faster Aruco detection strategy.
+    const int TAGDETECT_RIGHTCAM_CORNER_REFINE_METHOD    = cv::aruco::CORNER_REFINE_NONE;      // Algorithm used to refine tag corner pixels.
+    const bool TAGDETECT_RIGHTCAM_DETECT_INVERTED_MARKER = true;                               // Whether or not to detector upside-down tags.
+    const int TAGDETECT_RIGHTCAM_MARKER_BORDER_BITS      = 1;                                  // This number of bits on the border. A bit is one unit square of the tag.
+    const bool TAGDETECT_RIGHTCAM_USE_ARUCO3_DETECTION   = true;                               // Whether or not to use the newer and faster Aruco detection strategy.
 
     ///////////////////////////////////////////////////////////////////////////
     //// Object Detection Handler Adjustments.
