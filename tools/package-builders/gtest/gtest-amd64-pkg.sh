26cea3de1c9b9e65b863ca227093cedc454495b4
Eli Byrd <edbgkk@mst.edu>
2024-01-29 00:05:51 -0600

    Add other libraries

---
+++ tools/package-builders/gtest/gtest-amd64-pkg.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Set Working Directory
+cd /tmp
+
+# Install Variables
+GTEST_VERSION="v1.14.0"
+
+# Create Package Directory
+mkdir -p /tmp/pkg/googletest_${GTEST_VERSION}_amd64/usr/local
+mkdir -p /tmp/pkg/googletest_${GTEST_VERSION}_amd64/DEBIAN
+
+# Create Control File
+cat << EOF > /tmp/pkg/googletest_${GTEST_VERSION}_amd64/DEBIAN/control
+Package: googletest-mrdt
+Version: ${GTEST_VERSION}
+Maintainer: Google
+Depends: 
+Architecture: amd64
+Homepage: https://google.github.io/googletest/
+Description: A prebuilt version of Google Test. Made by the Mars Rover Design Team.
+EOF
+
+# Install Google Test
+git clone --depth 1 --branch ${GTEST_VERSION} https://github.com/google/googletest.git
+mkdir googletest/build
+cd googletest/build
+
+cmake ..
+
+make
+make install
+
+cd ../..
+rm -rf googletest
+
+# Create Package
+dpkg --build /tmp/pkg/googletest_${GTEST_VERSION}_amd64
+
+# Create Package Directory
+mkdir -p /tmp/pkg/deb
+
+# Copy Package
+cp /tmp/pkg/googletest_${GTEST_VERSION}_amd64.deb /tmp/pkg/deb/googletest_${GTEST_VERSION}_amd64.deb
---
+++ tools/package-builders/quill/quill-amd64-pkg.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Set Working Directory
+cd /tmp
+
+# Install Variables
+QUILL_VERSION="v3.6.0"
+
+# Create Package Directory
+mkdir -p /tmp/pkg/quill_${QUILL_VERSION}_amd64/usr/local
+mkdir -p /tmp/pkg/quill_${QUILL_VERSION}_amd64/DEBIAN
+
+# Create Control File
+cat << EOF > /tmp/pkg/quill_${QUILL_VERSION}_amd64/DEBIAN/control
+Package: quill-mrdt
+Version: ${QUILL_VERSION}
+Maintainer: odygrd
+Depends: 
+Architecture: amd64
+Homepage: https://quillcpp.readthedocs.io/en/latest/
+Description: A prebuilt version of Quill. Made by the Mars Rover Design Team.
+EOF
+
+# Install Google Test
+git clone --depth 1 --branch ${QUILL_VERSION} http://github.com/odygrd/quill.git
+mkdir quill/build
+cd quill/build
+
+cmake \ 
+-D CMAKE_BUILD_TYPE=Release ..
+
+make
+make install
+
+cd ../..
+rm -rf quill
+
+# Create Package
+dpkg --build /tmp/pkg/quill_${QUILL_VERSION}_amd64
+
+# Create Package Directory
+mkdir -p /tmp/pkg/deb
+
+# Copy Package
+cp /tmp/pkg/quill_${QUILL_VERSION}_amd64.deb /tmp/pkg/deb/quill_${QUILL_VERSION}_amd64.deb
