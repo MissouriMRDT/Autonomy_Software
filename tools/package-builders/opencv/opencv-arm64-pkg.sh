890daca3cbf315cf8a5009c9fb4581ef853df9c8
Eli Byrd <edbgkk@mst.edu>
2024-01-28 19:43:29 -0600

    Fix Action

--- tools/package-builders/opencv/opencv-arm64-pkg.sh
+++ tools/package-builders/opencv/opencv-arm64-pkg.sh
@@ -27,34 +27,33 @@ EOF
 # Install OpenCV
 git clone --depth 1 --branch ${OPENCV_VERSION} https://github.com/opencv/opencv.git
 git clone --depth 1 --branch ${OPENCV_VERSION} https://github.com/opencv/opencv_contrib.git
+ls
 mkdir opencv/build
 cd opencv/build
 
 cmake \
-    -D CMAKE_BUILD_TYPE=RELEASE \
-    -D CMAKE_INSTALL_PREFIX=/tmp/pkg/opencv_${OPENCV_VERSION}_amd64/usr/local \
-    -D INSTALL_PYTHON_EXAMPLES=OFF \
-    -D INSTALL_C_EXAMPLES=OFF \
-    -D BUILD_SHARED_LIBS=OFF \
-    -D BUILD_EXAMPLES=OFF \
-    -D BUILD_PERF_TESTS=OFF \
-    -D BUILD_TESTS=OFF \
-    -D WITH_CUDA=ON \
-    -D WITH_CUDNN=ON \
-    -D OPENCV_DNN_CUDA=ON \
-    -D WITH_VTK=OFF \
-    -D WITH_TBB=ON \
-    -D ENABLE_FAST_MATH=1 \
-    -D CUDA_FAST_MATH=1 \
-    ### Build for Jetson Orin GPU
-    -D CUDA_ARCH_PTX="" \
-    -D CUDA_ARCH_BIN="8.7" \
-    ###
-    -D WITH_CUBLAS=1 \
-    -D WITH_FFMPEG=ON \
-    -D OPENCV_EXTRA_MODULES_PATH=/tmp/opencv_contrib/modules/aruco \
-    -D OPENCV_EXTRA_MODULES_PATH=/tmp/opencv_contrib/modules/cudev \
-    -D HAVE_opencv_python3=ON ..
+-D CMAKE_BUILD_TYPE=RELEASE \
+-D CMAKE_INSTALL_PREFIX=/tmp/pkg/opencv_${OPENCV_VERSION}_amd64/usr/local \
+-D INSTALL_PYTHON_EXAMPLES=OFF \
+-D INSTALL_C_EXAMPLES=OFF \
+-D BUILD_SHARED_LIBS=OFF \
+-D BUILD_EXAMPLES=OFF \
+-D BUILD_PERF_TESTS=OFF \
+-D BUILD_TESTS=OFF \
+-D WITH_CUDA=ON \
+-D WITH_CUDNN=ON \
+-D OPENCV_DNN_CUDA=ON \
+-D WITH_VTK=OFF \
+-D WITH_TBB=ON \
+-D ENABLE_FAST_MATH=1 \
+-D CUDA_FAST_MATH=1 \
+-D CUDA_ARCH_PTX="" \
+-D CUDA_ARCH_BIN="8.7" \
+-D WITH_CUBLAS=1 \
+-D WITH_FFMPEG=ON \
+-D OPENCV_EXTRA_MODULES_PATH=/tmp/opencv_contrib/modules/aruco \
+-D OPENCV_EXTRA_MODULES_PATH=/tmp/opencv_contrib/modules/cudev \
+-D HAVE_opencv_python3=ON ..
 
 cat /proc/cpuinfo | grep "processor" | wc -l | xargs make -j
 make install
