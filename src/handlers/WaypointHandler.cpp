b84bce2de3fdcc5611234ffdbcdb78e192b584eb
clayjay3 <claytonraycowen@gmail.com>
2024-02-03 02:50:27 +0000

    Added waypoint handler class.

---
+++ src/handlers/WaypointHandler.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+ * @brief Implements the WaypointHandler class.
+ *
+ * @file WaypointHandler.cpp
+ * @author clayjay3 (claytonraycowen@gmail.com)
+ * @date 2024-02-02
+ *
+ * @copyright Copyright Mars Rover Design Team 2024 - All Rights Reserved
+ ******************************************************************************/
+
+#include "WaypointHandler.h"
+
+/******************************************************************************
+ * @brief Construct a new Waypoint Handler:: Waypoint Handler object.
+ *
+ *
+ * @author clayjay3 (claytonraycowen@gmail.com)
+ * @date 2024-02-02
+ ******************************************************************************/
+WaypointHandler::WaypointHandler()
+{
+    // Nothing to do yet.
+}
+
+/******************************************************************************
+ * @brief Destroy the Waypoint Handler:: Waypoint Handler object.
+ *
+ *
+ * @author clayjay3 (claytonraycowen@gmail.com)
+ * @date 2024-02-02
+ ******************************************************************************/
+WaypointHandler::~WaypointHandler()
+{
+    // Nothing to destroy.
+}
---
+++ src/handlers/WaypointHandler.h
@@ -0,0 +1,52 @@
+/******************************************************************************
+ * @brief Defines the WaypointHandler class.
+ *
+ * @file WaypointHandler.h
+ * @author clayjay3 (claytonraycowen@gmail.com)
+ * @date 2024-02-02
+ *
+ * @copyright Copyright Mars Rover Design Team 2024 - All Rights Reserved
+ ******************************************************************************/
+
+#ifndef WAYPOINT_HANDLER_H
+#define WAYPOINT_HANDLER_H
+
+/******************************************************************************
+ * @brief
+ *
+ *
+ * @author clayjay3 (claytonraycowen@gmail.com)
+ * @date 2024-02-02
+ ******************************************************************************/
+class WaypointHandler
+{
+    private:
+        /////////////////////////////////////////
+        // Declare private member variables.
+        /////////////////////////////////////////
+
+        /////////////////////////////////////////
+        // Declare private methods.
+        /////////////////////////////////////////
+
+    public:
+        /////////////////////////////////////////
+        // Declare public member variables.
+        /////////////////////////////////////////
+        WaypointHandler();
+        ~WaypointHandler();
+
+        /////////////////////////////////////////
+        // Declare public primary methods.
+        /////////////////////////////////////////
+
+        /////////////////////////////////////////
+        // Setters.
+        /////////////////////////////////////////
+
+        /////////////////////////////////////////
+        // Getters.
+        /////////////////////////////////////////
+};
+
+#endif
