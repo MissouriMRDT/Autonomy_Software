15765b1451533143db309629bc4582ac805d7ad1
JSpencerPittman <jspencerpittman@gmail.com>
2024-02-04 07:47:00 +0000

    Stanley Controller Template and Skeleton Calculation Method Implemented

--- src/algorithms/controllers/StanleyController.cpp
+++ src/algorithms/controllers/StanleyController.cpp
@@ -13,6 +13,8 @@
 /// \cond
 // Put implicit includes in here.
 
+#include <cmath>
+
 /// \endcond
 
 /******************************************************************************
@@ -29,13 +31,21 @@ namespace controllers
     /******************************************************************************
      * @brief Construct a new Stanley Contoller:: Stanley Contoller object.
      *
+     * @param dKp
+     * @param dDistToFrontAxle
+     * @param path
      *
-     * @author clayjay3 (claytonraycowen@gmail.com)
+     * @author clayjay3 (claytonraycowen@gmail.com),  JSpencerPittman (jspencerpittman@gmail.com)
      * @date 2024-02-01
      ******************************************************************************/
-    StanleyContoller::StanleyContoller()
+    StanleyContoller::StanleyContoller(const double dKp, const double dDistToFrontAxle, const double dYawTolerance, const std::vector<geoops::UTMCoordinate> vPathUTM)
     {
-        // Nothing to do yet.
+        // Initialize member variables
+        m_dKp              = dKp;
+        m_dDistToFrontAxle = dDistToFrontAxle;
+        m_dYawTolerance    = dYawTolerance;
+        m_unLastTargetIdx  = -1;
+        m_vPathUTM         = vPathUTM;
     }
 
     /******************************************************************************
@@ -48,5 +58,71 @@ namespace controllers
     StanleyContoller::~StanleyContoller()
     {
         // Nothing to destroy yet.
+    }
+
+    /******************************************************************************
+     * @brief Calculate the steering control adjustment for an agent using the Stanley method.
+     *
+     * This function computes the necessary change in yaw (steering angle) to align the agent with the predetermined path.
+     * The steering angle is limited by the proportional gain constant to prevent excessively sharp turns.
+     *
+     * @param utmCurrentPos the agent's current position in the UTM coordinate space.
+     * @param dVelocity the agent's current magnitude of velocity.
+     * @param dYaw the agent's current yaw angle (heading).
+     * @return The calculated change in yaw needed to align the agent with the path.
+     *
+     * @author JSpencerPittman (jspencerpittman@gmail.com)
+     * @date 2024-02-03
+     ******************************************************************************/
+    double StanleyContoller::Calculate(const geoops::UTMCoordinate utmCurrentPos, const double dVelocity, const double dYaw)
+    {
+        // Find the point on the path closest to the front axle center
+        unsigned int unTargetIdx = IdentifyTargetIdx(utmCurrentPos);
+
+        // Make sure the agent proceeds forward through the path
+        unTargetIdx       = std::max(unTargetIdx, m_unLastTargetIdx);
+        m_unLastTargetIdx = unTargetIdx;
+
+        // Calculate the change in yaw needed to go from the target to the next point
+        double dTargetYaw = CalculateTargetYaw(unTargetIdx);
+
+        // Calculate the difference between the rover's yaw and the desired path yaw
+        // TODO: Verify InputAngleModulus will work in this use case
+        double dYawError     = dTargetYaw - dYaw;
+        double dYawErrorNorm = numops::InputAngleModulus<double>(dYawError, -180.0, 180.0);
+
+        // Calculate the change in yaw needed to correct for the cross track error
+        double dCrossTrackError = CalculateCrossTrackError();
+        double dDeltaYaw        = dYawErrorNorm + std::atan2(m_dKp * dCrossTrackError, dVelocity);
+
+        // If a rotation is small enough we will just go ahead and skip it
+        if (dYawError < m_dYawTolerance)
+            dDeltaYaw = 0;
+
+        return dDeltaYaw;
+    }
+
+    /******************************************************************************
+     * @brief
+     *
+     *
+     * @author JSpencerPittman (jspencerpittman@gmail.com)
+     * @date 2024-02-03
+     ******************************************************************************/
+    unsigned int StanleyContoller::IdentifyTargetIdx(const geoops::UTMCoordinate utmPos) const
+    {
+        // TODO: Implement function
+    }
+
+    /******************************************************************************
+     * @brief
+     *
+     *
+     * @author JSpencerPittman (jspencerpittman@gmail.com)
+     * @date 2024-02-03
+     ******************************************************************************/
+    double StanleyContoller::CalculateTargetYaw(unsigned int unTargetIdx) const
+    {
+        // TODO: Implement function
     }
 }    // namespace controllers
