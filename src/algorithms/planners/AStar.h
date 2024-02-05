fea22e56d5aea6c5321eb0243444fe30aed70d1d
KaiShafe <kasq5m@umsystem.edu>
2024-02-05 18:40:55 +0000

    Minor changes, prep for merging waypoint handler

--- src/algorithms/planners/AStar.h
+++ src/algorithms/planners/AStar.h
@@ -13,6 +13,7 @@
 #define ASTAR_H
 
 #include "../../util/planners/Nodes.hpp"
+#include "../../vision/cameras/ZEDCam.h"
 
 /// \cond
 // Put implicit includes in here.
@@ -34,8 +35,8 @@ namespace pathplanners
      * @brief Implements the A* (ASTAR) algorithm with the ability to plan paths around
      *      obstacles and provide path bias points that the algorithm will try to adhere to.
      *
-     *
-     * @author
+     * @todo Make Obstacle into struct for readability.
+     * @author Kai Shafe (kasq5m@umsystem.edu)
      * @date 2024-02-01
      ******************************************************************************/
     class AStar
@@ -44,23 +45,33 @@ namespace pathplanners
             /////////////////////////////////////////
             // Declare private member variables.
             /////////////////////////////////////////
+            // Start and Goal Nodes
+            nodes::AStarNode m_StartNode;
+            nodes::AStarNode m_GoalNode;
+            // Nodes used as the path for routing (make this efficient using parent pointers)
             std::vector<nodes::AStarNode> m_vPathNodes;
+            // Obstacles for AStar to use during routing
+            // 
+            std::vector<std::pair<geoops::UTMCoordinate, float>> m_vObstacles;
+            // Multiplier for marking extra nodes around objects as obstacles
+            const float m_fAvoidanceMultiplier = 1.2;
 
             /////////////////////////////////////////
             // Declare private methods.
             /////////////////////////////////////////
+            void UpdateObstacles(std::vector<sl::ObjectData> vObstacles);
 
         public:
             /////////////////////////////////////////
             // Declare public member variables.
             /////////////////////////////////////////
-            AStar();
-            ~AStar();
 
             /////////////////////////////////////////
             // Declare public primary methods.
             /////////////////////////////////////////
-
+            AStar();
+            ~AStar();
+            void PlanAvoidancePath(std::vector<sl::ObjectData> vObstacles);
             /////////////////////////////////////////
             // Setters.
             /////////////////////////////////////////
