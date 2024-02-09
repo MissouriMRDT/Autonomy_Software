f085a860603927cf14fb3d7d25ba7013ef2cfe3e
clayjay3 <claytonraycowen@gmail.com>
2024-02-09 20:30:49 +0000

    Fix bazel build python errors.

--- tools/package-builders/tensorflow/tensorflow-amd64-pkg.sh
+++ tools/package-builders/tensorflow/tensorflow-amd64-pkg.sh
@@ -24,7 +24,7 @@ else
     curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
     mv bazel-archive-keyring.gpg /usr/share/keyrings
     echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-    apt update && apt install -y bazel-${TENSORFLOW_BAZEL_VERSION}
+    apt update && apt install -y bazel-${TENSORFLOW_BAZEL_VERSION} python3 python-is-python3
     # Symbolically link bazel-(version) install to /usr/bin/bazel.
     ln -s /usr/bin/bazel-${TENSORFLOW_BAZEL_VERSION} /usr/bin/bazel
     
