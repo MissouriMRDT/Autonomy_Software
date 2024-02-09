2b9dc2cbc19ad7cb08aa143a2bbc89b32e17fcca
clayjay3 <claytonraycowen@gmail.com>
2024-02-09 00:30:56 +0000

    Maybe fix action permission error.

--- tools/package-builders/tensorflow/tensorflow-arm64-pkg.sh
+++ tools/package-builders/tensorflow/tensorflow-arm64-pkg.sh
@@ -21,12 +21,12 @@ else
     echo "rebuilding_pkg=true" >> $GITHUB_OUTPUT
 
     # Install BAZEL build system.
-    curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
-    mv bazel-archive-keyring.gpg /usr/share/keyrings
-    echo "deb [arch=arm64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-    apt update && apt install -y bazel-${TENSORFLOW_BAZEL_VERSION}
+    sudo curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
+    sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
+    sudo echo "deb [arch=arm64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
+    sudo apt update && sudo apt install -y bazel-${TENSORFLOW_BAZEL_VERSION}
     # Symbolically link bazel-(version) install to /usr/bin/bazel.
-    ln -s /usr/bin/bazel-${TENSORFLOW_BAZEL_VERSION} /usr/bin/bazel
+    sudo ln -s /usr/bin/bazel-${TENSORFLOW_BAZEL_VERSION} /usr/bin/bazel
     
     # Delete Old Packages
     rm -rf /tmp/pkg
