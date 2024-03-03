20e878587112b4d593cf41b48459d48a4c5de5d0
Eli Byrd <edbgkk@mst.edu>
2024-03-03 15:35:33 -0600

    Move StateMachine States into h/cpp files

---
+++ src/states/ApproachingMarkerState.h
@@ -0,0 +1,48 @@
+/******************************************************************************
+ * @brief Approaching Marker State Implementation for Autonomy State Machine.
+ *
+ * @file ApproachingMarkerState.h
+ * @author Eli Byrd (edbgkk@mst.edu)
+ * @date 2024-03-03
+ *
+ * @copyright Copyright Mars Rover Design Team 2024 - All Rights Reserved
+ ******************************************************************************/
+
+#ifndef APPROACHINGMARKERSTATE_H
+#define APPROACHINGMARKERSTATE_H
+
+#include "../interfaces/State.hpp"
+
+/******************************************************************************
+ * @brief Namespace containing all state machine related classes.
+ *
+ * @author Eli Byrd (edbgkk@mst.edu)
+ * @date 2024-01-17
+ ******************************************************************************/
+namespace statemachine
+{
+    /******************************************************************************
+     * @brief The ApproachingMarkerState class implements the Approaching Marker
+     *        state for the Autonomy State Machine.
+     *
+     * @author Eli Byrd (edbgkk@mst.edu)
+     * @date 2024-01-17
+     ******************************************************************************/
+    class ApproachingMarkerState : public State
+    {
+        private:
+            int m_nNumDetectionAttempts;
+            bool m_bInitialized;
+
+        protected:
+            void Start() override;
+            void Exit() override;
+
+        public:
+            ApproachingMarkerState();
+            States Run() override;
+            States TriggerEvent(Event eEvent) override;
+    };
+}    // namespace statemachine
+
+#endif    // APPROACHINGMARKERSTATE_H
---
+++ src/states/NavigatingState.h
@@ -0,0 +1,52 @@
+/******************************************************************************
+ * @brief Navigating State Implementation for Autonomy State Machine.
+ *
+ * @file NavigatingState.h
+ * @author Eli Byrd (edbgkk@mst.edu)
+ * @date 2024-03-03
+ *
+ * @copyright Copyright Mars Rover Design Team 2024 - All Rights Reserved
+ ******************************************************************************/
+
+#ifndef NAVIGATINGSTATE_H
+#define NAVIGATINGSTATE_H
+
+#include "../interfaces/State.hpp"
+
+/******************************************************************************
+ * @brief Namespace containing all state machine related classes.
+ *
+ * @author Eli Byrd (edbgkk@mst.edu)
+ * @date 2024-01-17
+ ******************************************************************************/
+namespace statemachine
+{
+    /******************************************************************************
+     * @brief The NavigatingState class implements the Navigating state for the Autonomy
+     *        State Machine.
+     *
+     * @author Eli Byrd (edbgkk@mst.edu)
+     * @date 2024-01-17
+     ******************************************************************************/
+    class NavigatingState : public State
+    {
+        private:
+            int m_nMaxDataPoints;
+            std::vector<double> m_vRoverXPosition;
+            std::vector<double> m_vRoverYPosition;
+            time_t m_tStuckCheckTime;
+            double m_dStuckCheckLastPosition[2];
+            bool m_bInitialized;
+
+        protected:
+            void Start() override;
+            void Exit() override;
+
+        public:
+            NavigatingState();
+            States Run() override;
+            States TriggerEvent(Event eEvent) override;
+    };
+}    // namespace statemachine
+
+#endif    // NAVIGATINGSTATE_H
