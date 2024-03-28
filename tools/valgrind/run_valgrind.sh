3a9ab7760f24c88cd3b1bf64b2769ccc24ba14c0
Eli Byrd <edbgkk@mst.edu>
2024-03-28 02:37:11 +0000

    Update Valgrind Script

--- tools/valgrind/run_valgrind.sh
+++ tools/valgrind/run_valgrind.sh
@@ -26,6 +26,8 @@ supp_files=$(find "$script_dir" -maxdepth 1 -type f -name "*.supp")
 valgrind_cmd="valgrind -s --leak-check=yes --show-leak-kinds=all --track-origins=yes "
 if [ "$1" = "GitHub-Action" ]; then
   valgrind_cmd+=" --log-file=/opt/Autonomy_Software/tools/valgrind/valgrind.rpt"
+else
+  valgrind_cmd+=" --log-file=/workspaces/Autonomy_Software/tools/valgrind/valgrind.rpt"
 fi
 for supp_file in $supp_files; do
   valgrind_cmd+=" --suppressions=$supp_file"
@@ -33,4 +35,4 @@ done
 valgrind_cmd+=" $executable_path"
 
 # Run the Valgrind command
-eval $valgrind_cmd
+timeout --signal=SIGINT 60s $valgrind_cmd
