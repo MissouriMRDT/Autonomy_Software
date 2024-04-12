4f9c09750ef5fe3ce128a0b6da9da15d931374dc
clayjay3 <claytonraycowen@gmail.com>
2024-04-12 19:20:28 +0000

    Fix tag struct safety

--- src/vision/aruco/ArucoDetection.hpp
+++ src/vision/aruco/ArucoDetection.hpp
@@ -49,7 +49,7 @@ namespace arucotag
             cv::Point2f CornerTR;                                                                       // The top right corner of the bounding box.
             cv::Point2f CornerBL;                                                                       // The bottom left corner of the bounding box.
             cv::Point2f CornerBR;                                                                       // The bottom right corner of bounding box.
-            const std::vector<cv::Point2f*> vCorners = {&CornerTL, &CornerTR, &CornerBL, &CornerBR};    // Provide an easy method for getting all corners.
+            std::vector<const cv::Point2f*> vCorners = {&CornerTL, &CornerTR, &CornerBL, &CornerBR};    // Provide an easy method for getting all corners.
             int nID;                                                                                    // ID of the tag.
             int nHits;                                                                                  // Total number of detections for tag id.
             int nFramesSinceLastHit;         // The total number of frames since a tag with this ID was last detected.
@@ -72,7 +72,7 @@ namespace arucotag
         cv::Point2f cvCenter(0, 0);
 
         // Loop through each corner of the tag.
-        for (cv::Point2f* cvCorner : stTag.vCorners)
+        for (const cv::Point2f* cvCorner : stTag.vCorners)
         {
             // Add each tag x, y to the center x, y.
             cvCenter.x += cvCorner->x;
