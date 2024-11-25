665b6cf1ad65fba98867fe2b960b4aeae6d7b2a1
clayjay3 <claytonraycowen@gmail.com>
2024-11-25 09:55:08 -0600

    Increase test coverage for PIDController.

--- tests/Unit/src/algorithms/controllers/PIDController.cc
+++ tests/Unit/src/algorithms/controllers/PIDController.cc
@@ -71,6 +71,8 @@ TEST(PIDControllerTest, ProportionalControl)
     {
         // Calculate drive powers.
         double dOutput = pPIDController->Calculate(aActualInput[nIter], aSetpointInput[nIter]);
+        dOutput        = pPIDController->Calculate(aActualInput[nIter]);
+        dOutput        = pPIDController->Calculate();
 
         // Check that the expected output values were calculated.
         EXPECT_NEAR(dOutput, aExpectedOutput[nIter], 0.01);    // Left output check.
@@ -98,14 +100,15 @@ TEST(PIDControllerTest, IntegralControl)
     const int nTestValuesLength                     = 5;
     const double aActualInput[nTestValuesLength]    = {0.0, 1.0, 2.0, 3.0, -50.0};
     const double aSetpointInput[nTestValuesLength]  = {0.0, 1.0, 1.0, 1.0, -49.5};
-    const double aExpectedOutput[nTestValuesLength] = {0.0, 0.0, -1.0, -4.0, -5.5};
+    const double aExpectedOutput[nTestValuesLength] = {0.0, 0.0, -2.0, -7.0, -8.0};
 
     // Loop through each value and compare inputs and outputs.
     for (int nIter = 0; nIter < nTestValuesLength; ++nIter)
     {
         // Calculate drive powers a few times.
         double dOutput = pPIDController->Calculate(aActualInput[nIter], aSetpointInput[nIter]);
-        dOutput        = pPIDController->Calculate(aActualInput[nIter], aSetpointInput[nIter]);
+        dOutput        = pPIDController->Calculate(aActualInput[nIter]);
+        dOutput        = pPIDController->Calculate();
 
         // Check that the expected output values were calculated.
         EXPECT_NEAR(dOutput, aExpectedOutput[nIter], 0.01);    // Left output check.
@@ -149,6 +152,158 @@ TEST(PIDControllerTest, DerivativeControl)
         // Check that the expected output values were calculated.
         EXPECT_NEAR(dOutput, aExpectedOutput[nIter], 0.01);    // Left output check.
     }
+
+    // Delete object.
+    delete pPIDController;
+    // Point to null.
+    pPIDController = nullptr;
+}
+
+/******************************************************************************
+ * @brief Test PIDController limits adherence.
+ *
+ *
+ * @author clayjay3 (claytonraycowen@gmail.com)
+ * @date 2024-11-24
+ ******************************************************************************/
+TEST(PIDControllerTest, ControllerLimits)
+{
+    // Create a new PIDController object.
+    controllers::PIDController* pPIDController = new controllers::PIDController(1.0, 0.0, 0.0);
+    // Set controller limits.
+    pPIDController->SetMaxSetpointDifference(0.1);
+    pPIDController->SetMaxIntegralEffort(1);
+    pPIDController->SetOutputLimits(0.1);
+    pPIDController->SetOutputRampRate(0.01);
+
+    // Create array for storing input and expect output values.
+    const int nTestValuesLength                     = 5;
+    const double aActualInput[nTestValuesLength]    = {0.0, 1.0, 2.0, 3.0, -50.0};
+    const double aSetpointInput[nTestValuesLength]  = {0.0, 1.0, 1.0, 1.0, -49.5};
+    const double aExpectedOutput[nTestValuesLength] = {0.0, -0.01, -0.01, -0.02, -0.01};
+
+    // Loop through each value and compare inputs and outputs.
+    for (int nIter = 0; nIter < nTestValuesLength; ++nIter)
+    {
+        // Calculate drive powers.
+        double dOutput = pPIDController->Calculate(aActualInput[nIter], aSetpointInput[nIter]);
+
+        // Check that the expected output values were calculated.
+        EXPECT_NEAR(dOutput, aExpectedOutput[nIter], 0.01);    // Left output check.
+    }
+
+    // Delete object.
+    delete pPIDController;
+    // Point to null.
+    pPIDController = nullptr;
+}
+
+/******************************************************************************
+ * @brief Test PIDController wrap around for rotational positioning.
+ *
+ *
+ * @author clayjay3 (claytonraycowen@gmail.com)
+ * @date 2024-11-24
+ ******************************************************************************/
+TEST(PIDControllerTest, ContinuousInput)
+{
+    // Create a new PIDController object.
+    controllers::PIDController* pPIDController = new controllers::PIDController(1.0, 0.0, 0.0);
+    // Enable continuous input.
+    pPIDController->EnableContinuousInput(-180.0, 180.0);
+
+    // Create array for storing input and expect output values.
+    const int nTestValuesLength                     = 5;
+    const double aActualInput[nTestValuesLength]    = {0.0, 1.0, 2.0, 3.0, -50.0};
+    const double aSetpointInput[nTestValuesLength]  = {0.0, 1.0, 1.0, 1.0, -49.5};
+    const double aExpectedOutput[nTestValuesLength] = {0.0, 0.0, -1.0, -2.0, 0.5};
+
+    // Loop through each value and compare inputs and outputs.
+    for (int nIter = 0; nIter < nTestValuesLength; ++nIter)
+    {
+        // Calculate drive powers.
+        double dOutput = pPIDController->Calculate(aActualInput[nIter], aSetpointInput[nIter]);
+
+        // Check that the expected output values were calculated.
+        EXPECT_NEAR(dOutput, aExpectedOutput[nIter], 0.01);    // Left output check.
+    }
+
+    // Disable continuous input.
+    pPIDController->DisableContinuousInput();
+    // Reset controller.
+    pPIDController->Reset();
+
+    // Create array for storing input and expect output values.
+    const double aExpectedOutput2[nTestValuesLength] = {0.0, 0.0, -1.0, -2.0, 0.5};
+
+    // Loop through each value and compare inputs and outputs.
+    for (int nIter = 0; nIter < nTestValuesLength; ++nIter)
+    {
+        // Calculate drive powers.
+        double dOutput = pPIDController->Calculate(aActualInput[nIter], aSetpointInput[nIter]);
+
+        // Check that the expected output values were calculated.
+        EXPECT_NEAR(dOutput, aExpectedOutput2[nIter], 0.01);    // Left output check.
+    }
+
+    // Delete object.
+    delete pPIDController;
+    // Point to null.
+    pPIDController = nullptr;
+}
+
+/******************************************************************************
+ * @brief Test PIDController mutators and accessors.
+ *
+ *
+ * @author clayjay3 (claytonraycowen@gmail.com)
+ * @date 2024-11-25
+ ******************************************************************************/
+TEST(PIDControllerTest, MutatorsAndAccessors)
+{
+    // Create a new PIDController object.
+    controllers::PIDController* pPIDController = new controllers::PIDController(1.0, 0.0, 0.0);
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Test mutators and then see if accessors return the same values.
+    ////////////////////////////////////////////////////////////////////////////
+
+    // PID gains.
+    pPIDController->SetProportional(2.0);
+    EXPECT_NEAR(pPIDController->GetProportional(), 2.0, 0.01);
+    pPIDController->SetIntegral(3.0);
+    EXPECT_NEAR(pPIDController->GetIntegral(), 3.0, 0.01);
+    pPIDController->SetDerivative(4.0);
+    EXPECT_NEAR(pPIDController->GetDerivative(), 4.0, 0.01);
+    pPIDController->SetFeedforward(5.0);
+    EXPECT_NEAR(pPIDController->GetFeedforward(), 5.0, 0.01);
+    pPIDController->SetPID(6.0, 7.0, 8.0);
+    EXPECT_NEAR(pPIDController->GetProportional(), 6.0, 0.01);
+    EXPECT_NEAR(pPIDController->GetIntegral(), 7.0, 0.01);
+    EXPECT_NEAR(pPIDController->GetDerivative(), 8.0, 0.01);
+    pPIDController->SetPID(9.0, 10.0, 11.0, 12.0);
+    EXPECT_NEAR(pPIDController->GetProportional(), 9.0, 0.01);
+    EXPECT_NEAR(pPIDController->GetIntegral(), 10.0, 0.01);
+    EXPECT_NEAR(pPIDController->GetDerivative(), 11.0, 0.01);
+    EXPECT_NEAR(pPIDController->GetFeedforward(), 12.0, 0.01);
+
+    // Other settings.
+    pPIDController->SetSetpoint(6.0);
+    EXPECT_NEAR(pPIDController->GetSetpoint(), 6.0, 0.01);
+    pPIDController->SetMaxSetpointDifference(7.0);
+    EXPECT_NEAR(pPIDController->GetMaxSetpointDifference(), 7.0, 0.01);
+    pPIDController->SetMaxIntegralEffort(8.0);
+    EXPECT_NEAR(pPIDController->GetMaxIntegralEffort(), 8.0, 0.01);
+    pPIDController->SetOutputRampRate(9.0);
+    EXPECT_NEAR(pPIDController->GetOutputRampRate(), 9.0, 0.01);
+    pPIDController->SetOutputFilter(0.1);
+    EXPECT_NEAR(pPIDController->GetOutputFilter(), 0.1, 0.01);
+    pPIDController->SetDirection(true);
+    EXPECT_TRUE(pPIDController->GetReversed());
+
+    // No accessors for these.
+    pPIDController->SetOutputLimits(11.0, 12.0);
+    pPIDController->SetOutputLimits(13.0);
 
     // Delete object.
     delete pPIDController;
