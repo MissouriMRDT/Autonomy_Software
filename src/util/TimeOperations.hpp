974fa41bcbe97bece4528add072cd66302de3bc9
Eli Byrd <edbgkk@mst.edu>
2025-01-07 14:29:27 -0600

    Log to Quill if enabled otherwise standard out

--- src/util/TimeOperations.hpp
+++ src/util/TimeOperations.hpp
@@ -52,7 +52,15 @@ namespace timeops
         siTimeCharacters = std::strftime(cCurrentTime.data(), cCurrentTime.size(), szFormat.c_str(), tLocalTime);
         if (siTimeCharacters == 0)
         {
-            std::cerr << "Unable to format calendar date & time (exceeds string length)" << std::endl;
+            // Check if Quill has been initialized. If so, log the error. Otherwise, print to stderr.
+            if (logging::g_qSharedLogger != nullptr)
+            {
+                LOG_CRITICAL(logging::g_qSharedLogger, "Unable to format calendar date & time (exceeds string length)");
+            }
+            else
+            {
+                std::cerr << "Unable to format calendar date & time (exceeds string length)" << std::endl;
+            }
         }
 
         return cCurrentTime.data();
