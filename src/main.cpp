f8f174df4ccd7c94fbc7589ebebb007b1d6ed4c0
clayjay3 <claytonraycowen@gmail.com>
2025-03-05 18:57:47 -0600

    Remove example from main.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@
  * @copyright Copyright Mars Rover Design Team 2023 - All Rights Reserved
  ******************************************************************************/
 
-#include "../examples/vision/pcl/ZEDSLAM.hpp"
 #include "./AutonomyGlobals.h"
 #include "./AutonomyLogging.h"
 #include "./AutonomyNetworking.h"
