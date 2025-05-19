677f57a5dcec3ea21898a105eb37fecdb238e73d
frawgii <samanthahajdukiewicz@gmail.com>
2025-05-19 21:30:59 +0000

    Added new mallet-bottle dataset

--- src/AutonomyConstants.cpp
+++ src/AutonomyConstants.cpp
@@ -232,9 +232,9 @@ namespace constants
     const int OBJECTDETECT_MAINCAM_MAX_FPS                = 30;      // The max iterations per second of the tag detector.
     const bool OBJECTDETECT_MAINCAM_ENABLE_TORCH          = true;    // Whether or not to use pytorch detection on top of ArUco.
     const std::string OBJECTDETECT_MAINCAM_TORCH_MODEL =
-        "../data/models/yolo_models/bottle_mallet/v8n_x640_200epochs/best.torchscript";    // The model path to use for detection.
-    const float OBJECTDETECT_MAINCAM_TORCH_CONFIDENCE = 0.4f;                              // The minimum confidence to consider a viable AR tag detection.
-    const float OBJECTDETECT_MAINCAM_TORCH_NMS_THRESH = 0.4f;                              // The threshold for non-max suppression filtering.
+        "../data/models/yolo_models/bottle_mallet_new/v8n_x640_200epochs/best.torchscript";    // The model path to use for detection.
+    const float OBJECTDETECT_MAINCAM_TORCH_CONFIDENCE = 0.4f;                                  // The minimum confidence to consider a viable AR tag detection.
+    const float OBJECTDETECT_MAINCAM_TORCH_NMS_THRESH = 0.4f;                                  // The threshold for non-max suppression filtering.
 
     ///////////////////////////////////////////////////////////////////////////
 
