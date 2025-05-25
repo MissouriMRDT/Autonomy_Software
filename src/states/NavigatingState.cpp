7a568b731ab3627b4be8b1999c791aeb900aa646
clayjay3 <claytonraycowen@gmail.com>
2025-05-25 17:31:31 -0500

    Cleanup and future proofing.

--- src/states/NavigatingState.cpp
+++ src/states/NavigatingState.cpp
@@ -195,7 +195,8 @@ namespace statemachine
                 case geoops::WaypointType::eNavigationWaypoint:
                 {
                     // Continuously navigate to the next waypoint if our current waypoint ID is set to -99.
-                    if (globals::g_pWaypointHandler->GetWaypointCount() > 1 && m_stGoalWaypoint.nID == -99)
+                    if (globals::g_pWaypointHandler->GetWaypointCount() > 1 &&
+                        m_stGoalWaypoint.nID == static_cast<int>(manifest::Autonomy::AUTONOMYWAYPOINTTYPES::CONTINUOUSNAVIGATE))
                     {
                         // Submit logger message.
                         LOG_NOTICE(logging::g_qSharedLogger, "NavigatingState: The current waypoint ID is {}. Continuing to next waypoint...", m_stGoalWaypoint.nID);
@@ -431,24 +432,24 @@ namespace statemachine
                     // Add starting point and goal point to path plot.
                     m_pRoverPathPlot->AddPathPoint(globals::g_pWaypointHandler->SmartRetrieveRoverPose().GetUTMCoordinate(), "NavPath", 0);
                     m_pRoverPathPlot->AddPathPoint(m_stGoalWaypoint, "NavPath", 0);
-                }
 
-                // Get all obstacles from the obstacle handler.
-                std::vector<geoops::Waypoint> vObstacles = globals::g_pWaypointHandler->GetAllObstacles();
-                // Add obstacles to the A* planner.
-                m_pAStarPlanner->UpsertObstacleData(vObstacles);
-                // Set A* planner start and goal.
-                m_vPathCoordinates =
-                    m_pAStarPlanner->PlanAvoidancePath(globals::g_pWaypointHandler->SmartRetrieveRoverPose().GetUTMCoordinate(), m_stGoalWaypoint.GetUTMCoordinate());
-                // Set the path of the stanley controller.
-                m_pStanleyController->SetReferencePath(m_vPathCoordinates);
-                // Get the smoothed path for plotting.
-                std::vector<geoops::Waypoint> vSmoothedPath = m_pStanleyController->GetReferencePath();
-                // Update our plot with the new path.
-                m_pRoverPathPlot->ClearLayer("AStarPath");
-                m_pRoverPathPlot->AddPathPoints(m_vPathCoordinates, "AStarPath", 0);
-                m_pRoverPathPlot->AddDots(vSmoothedPath, "SmoothPath", 0);
-                m_pRoverPathPlot->AddDots(vObstacles, "ObstaclesLocation", 0);
+                    // Get all obstacles from the obstacle handler.
+                    std::vector<geoops::Waypoint> vObstacles = globals::g_pWaypointHandler->GetAllObstacles();
+                    // Add obstacles to the A* planner.
+                    m_pAStarPlanner->UpsertObstacleData(vObstacles);
+                    // Set A* planner start and goal.
+                    m_vPathCoordinates =
+                        m_pAStarPlanner->PlanAvoidancePath(globals::g_pWaypointHandler->SmartRetrieveRoverPose().GetUTMCoordinate(), m_stGoalWaypoint.GetUTMCoordinate());
+                    // Set the path of the stanley controller.
+                    m_pStanleyController->SetReferencePath(m_vPathCoordinates);
+                    // Get the smoothed path for plotting.
+                    std::vector<geoops::Waypoint> vSmoothedPath = m_pStanleyController->GetReferencePath();
+                    // Update our plot with the new path.
+                    m_pRoverPathPlot->ClearLayer("AStarPath");
+                    m_pRoverPathPlot->AddPathPoints(m_vPathCoordinates, "AStarPath", 0);
+                    m_pRoverPathPlot->AddDots(vSmoothedPath, "SmoothPath", 0);
+                    m_pRoverPathPlot->AddDots(vObstacles, "ObstaclesLocation", 0);
+                }
 
                 // Send multimedia command to update state display.
                 globals::g_pMultimediaBoard->SendLightingState(MultimediaBoard::MultimediaBoardLightingState::eAutonomy);
