cce5ff4726491c5e185f4096a0a0cbe3c567e15d
clayjay3 <claytonraycowen@gmail.com>
2026-01-14 11:44:00 -0600

    Changes from testing

--- src/AutonomyConstants.cpp
+++ src/AutonomyConstants.cpp
@@ -257,7 +257,7 @@ namespace constants
     ///////////////////////////////////////////////////////////////////////////
 
     // LiDAR Data Handler.
-    const std::string LIDAR_HANDLER_DB_PATH = "../data/LiDAR/data/databases/Flat_SIM.db";    // The path to the LiDAR database file.
+    const std::string LIDAR_HANDLER_DB_PATH = "../data/LiDAR/data/databases/Rolla.db";    // The path to the LiDAR database file.
 
     ///////////////////////////////////////////////////////////////////////////
 
