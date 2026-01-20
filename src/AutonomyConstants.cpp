9c4f76554e3e40f2adb4855d409b0bce67c9515f
Sam Hajdukiewicz <samanthahajdukiewicz@gmail.com>
2026-01-20 14:07:44 -0600

    Added new BMP model.

--- src/AutonomyConstants.cpp
+++ src/AutonomyConstants.cpp
@@ -239,7 +239,7 @@ namespace constants
     const int OBJECTDETECT_MAINCAM_MAX_FPS                = 30;      // The max iterations per second of the object detector.
     const bool OBJECTDETECT_MAINCAM_ENABLE_TORCH          = true;    // Whether or not to use pytorch detection.
     const std::string OBJECTDETECT_MAINCAM_TORCH_MODEL =
-        "../data/models/yolo_models/bmp_v1/v8n_x640_200epochs/best.torchscript";    // The model path to use for detection.
+        "../data/models/yolo_models/bmp_v4/v8n_x640_200epochs/best.torchscript";    // The model path to use for detection.
     const float OBJECTDETECT_MAINCAM_TORCH_CONFIDENCE = 0.7f;                       // The minimum confidence to consider a viable object detection.
     const float OBJECTDETECT_MAINCAM_TORCH_NMS_THRESH = 0.4f;                       // The threshold for non-max suppression filtering.
 
