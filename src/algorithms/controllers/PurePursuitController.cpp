99d088486f7cfc454a312dd3028bcac4b31868ad
Aiden Buter <aiden.buter@gmail.com>
2026-04-19 15:17:39 +0000

    updated circling fix

--- src/algorithms/controllers/PurePursuitController.cpp
+++ src/algorithms/controllers/PurePursuitController.cpp
@@ -239,22 +239,13 @@ namespace controllers
             double dy                        = stCurrentPosition.dNorthing - stA.dNorthing;
             double dDistSq                   = dx * dx + dy * dy;
 
-            // TODO: move to appropriate location
+            // TODO: move to appropriate location.
             double kCloseRangePenalty = 0.2;
 
             if (dDistSq < dClosestDistanceSq)
             {
-                if (dDistSq < kCloseRangePenalty)
-                {
-                    dClosestDistanceSq = dDistSq + kCloseRangePenalty;
-                }
-
-                else
-                {
-                    dClosestDistanceSq = dDistSq;
-                }
-
-                nBestSegmentIndex = static_cast<int>(siIter);
+                dClosestDistanceSq = std::max(dDistSq, kCloseRangePenalty);
+                nBestSegmentIndex  = static_cast<int>(siIter);
             }
         }
 
