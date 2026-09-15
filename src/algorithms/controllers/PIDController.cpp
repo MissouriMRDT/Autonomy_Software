00e6fca118794bd54b2fc6876addfd9f54d2deff
Aiden Buter <aiden.buter@gmail.com>
2026-09-15 01:17:06 +0000

    Velocity PID blueprint

--- src/algorithms/controllers/PIDController.cpp
+++ src/algorithms/controllers/PIDController.cpp
@@ -217,6 +217,144 @@ namespace controllers
         return this->Calculate(dActual, m_dSetpoint);
     }
 
+    /**************************************************************************
+     * @brief Calculate the next control output given the current veloctiy and a goal
+     *      velocity. 
+     *
+     * @param dCurrVelo - The current actual velocity of the system.
+     * @param dGoalVelo - The desired goal velocity of the controller.
+     * @return double - The resultant PID controller output.
+     *
+     * @author ajbuter (aiden.buter@gmail.com)
+     * @date 2026-09-14
+     **************************************************************************/
+    double PIDController::CalculateVelocity(const double dCurrVelo, const double dGoalVelo)
+    {
+        // Create instance variables.
+        double dPTermOutput;
+        double dITermOutput;
+        double dDTermOutput;
+        double dFFTermOutput;
+        double dOutput;
+
+        // Update setpoint member variable.
+        m_dSetpoint = dGoalVelo;
+
+        // Apply the sliding setpoint range adjust if necessary.
+        if (m_dMaxSetpointDifference != 0)
+        {
+            // Clamp current setpoint value within a certain range of the current velocity.
+            m_dSetpoint = numops::Clamp(m_dSetpoint, dCurrVelo - m_dMaxSetpointDifference, dCurrVelo + m_dMaxSetpointDifference);
+        }
+
+        // Determine error from setpoint.
+        double dError = m_dSetpoint - dCurrVelo;
+        // Check if the controller is within the tolerance.
+        if (numops::Bounded(dError, -m_dTolerance, m_dTolerance))
+        {
+            // Reset the error sum.
+            dError = 0.0;
+        }
+
+        // Calculate feedforward term.
+        dFFTermOutput = m_dKff * dGoalVelo;
+        // Calculate proportional term.
+        dPTermOutput = m_dKp * dError;
+        // Check if this is the first time doing a calculation.
+        if (m_bFirstCalculation)
+        {
+            // Assume the process variable hold same as previous.
+            m_dLastError = dError;
+            // Assume the last output is the current time-independent output.
+            m_dLastControlOutput = dPTermOutput + dFFTermOutput;
+            // Set first calculation toggle to false.
+            m_bFirstCalculation = false;
+        }
+        // Calculate derivative term.
+        // Note, derivative is actually negative and "slows" the system if it's doing
+        // the correct thing. Small gain values help prevent output spikes and overshoot.
+        dDTermOutput = -m_dKd * (dError - m_dLastError);
+        m_dLastError = dError;
+        // Calculate integral term.
+        // The integral term is more complex. There's several things to factor in to make it easier to deal with.
+        // 1. m_dMaxIEffort restricts the amount of output contributed by the integral term.
+        // 2. prevent windup by not increasing errorSum if we're already running against our max Ioutput
+        // 3. prevent windup by not increasing errorSum if output is output=maxOutput
+        dITermOutput = m_dKi * m_dErrorSum;
+        // Apply integral term limits.
+        if (m_dMaxIEffort != 0)
+        {
+            // Clamp current integral term output.
+            dITermOutput = numops::Clamp(dITermOutput, -m_dMaxIEffort, m_dMaxIEffort);
+        }
+        // Here's PID!
+        dOutput = dFFTermOutput + dPTermOutput + dITermOutput + dDTermOutput;
+        // Check if error is at not at limits.
+        if (m_dMinEffort != m_dMaxEffort && !numops::Bounded(dOutput, m_dMinEffort, m_dMaxEffort))
+        {
+            // Reset the error to a sane level. Settings to the current error ensures a smooth transition when the P term
+            // decreases enough for the I term to start acting upon the controller properly.
+            m_dErrorSum = dError;
+        }
+        // Check if the error is within the allowed ramp rate compared to the last output.
+        else if (m_dOutputRampRate != 0 && !numops::Bounded(dOutput, m_dLastControlOutput - m_dOutputRampRate, m_dLastControlOutput + m_dOutputRampRate))
+        {
+            // Reset the error to a sane level. Settings to the current error ensures a smooth transition when the P term
+            // decreases enough for the I term to start acting upon the controller properly.
+            m_dErrorSum = dError;
+        }
+        // Check if integral term output should be limited.
+        else if (m_dMaxIEffort != 0)
+        {
+            // In addition to output limiting directly, we also want to prevent integral term wind-up. Restrict directly.
+            m_dErrorSum = numops::Clamp(m_dErrorSum + dError, -m_dMaxIError, m_dMaxIError);
+        }
+        else
+        {
+            // Accumulate error.
+            m_dErrorSum += dError;
+        }
+        // Restrict output to our specified ramp limits.
+        if (m_dOutputRampRate != 0)
+        {
+            // Clamp output to ramp rate.
+            dOutput = numops::Clamp(dOutput, m_dLastControlOutput - m_dOutputRampRate, m_dLastControlOutput + m_dOutputRampRate);
+        }
+        // Restrict output to our specified effort limits.
+        if (m_dMinEffort != m_dMaxEffort)
+        {
+            // Clamp output to effort limits.
+            dOutput = numops::Clamp(dOutput, m_dMinEffort, m_dMaxEffort);
+        }
+        // Reduce jitters and output spikes with an exponential rolling sum filter.
+        if (m_dOutputFilter != 0)
+        {
+            // Filter output effort.
+            dOutput = (m_dLastControlOutput * m_dOutputFilter) + (dOutput * (1 - m_dOutputFilter));
+        }
+        // Update last output member variable.
+        m_dLastControlOutput = dOutput;
+
+        // Return the PID controller calculated output effort.
+        return dOutput;
+    }
+
+        /**************************************************************************
+     * @brief Calculate the next control output given the current velocity and
+     *      using the previously set setpoint.
+     *
+     * @param dCurrVelo - The current actual velocity of the system.
+     * @return double - The resultant PID controller output.
+     *
+     * @author ajbuter (aiden.buter@gmail.com)
+     * @date 2026-09-14
+     **************************************************************************/
+    double PIDController::CalculateVelocity(const double dCurrVelo)
+    {
+        // Calculate and return the output from the PIDController using the same setpoint.
+        return this->CalculateVelocity(dCurrVelo, m_dSetpoint);
+    }
+
     /******************************************************************************
      * @brief Enables continuous wraparound of PID controller input. Use for setpoints
      *      that exists on a circle. For example, if using degrees for setpoint and actual
